3907dfbb50077ac97183c744de210156176b241c
JJYNG1023 <j.yeung2020@outlook.com>
2026-04-21 22:08:50 +0100

    add user request look to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -18,13 +18,17 @@ def get_sales_data():
     Get sales figures input from the user.
     Data must be six numbers separated by commas.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True: 
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
 def validate_data(values):
     """inside the try, converts all string values into interers. Raises value error if string cannot be converted into int, or if there aren't exactly 6 values"""
@@ -36,5 +40,8 @@ def validate_data(values):
             )
     except ValueError as e:
             print(f"invalid data: {e}, please try again.\n")
+            return False
+
+    return True
 
 get_sales_data()
