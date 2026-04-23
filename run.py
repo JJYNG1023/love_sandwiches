98d07f7b84f2da8f05a00c79c61a90bed509512a
JJYNG1023 <j.yeung2020@outlook.com>
2026-04-23 21:43:56 +0100

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -39,7 +39,7 @@ def validate_data(values):
         [int(value) for value in values]  
         if len(values) != 6:
             raise ValueError(
-                f"excatly 6 values required, you orivuded {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
             print(f"invalid data: {e}, please try again.\n")
@@ -47,19 +47,12 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
-    """update sales worksheet, add new row with the list data provided"""
-    print("updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated successfully.\n")
-
-def update_surplus_worksheet(data):
-    """update surplus worksheet, add new row with the list data provided"""
-    print("updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated successfully.\n")
+def update_worksheet(data, worksheet):
+    """update any worksheet, add new row with the list data provided"""
+    print( f" updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -80,10 +73,10 @@ def main():
     """Run all program functions"""
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data , "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
